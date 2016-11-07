2b8a8636817f6e29c54b7d302568e9d53905ee95
lucas-mqs <lucas.souza.marques@ig.com.br>
2016-11-07 18:38:41 -0200

    Inicio do Adapter para percursos

---
+++ src/main/java/br/com/sovrau/adapters/PercursoArrayAdapter.java
@@ -0,0 +1,8 @@
+package br.com.sovrau.adapters;
+/**
+ * Created by Lucas.Marques on 07/11/2016.
+ */
+public class PercursoArrayAdapter extends ArrayAdapter<PercursoDTO> {
+    private Activit
+
+}
